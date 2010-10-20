376955e38cae1b25ca39d1cad5c122b5450a9b47
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-10-20 14:03:57 -0200

    Add logging functions
    Based on logging functions from systemd-bootstatus at
    http://git.profusion.mobi/cgit.cgi/gustavo/systemd-bootstatus

---
+++ src/log.c
@@ -0,0 +1,70 @@
+#include "log.h"
+
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+static const char *_log_level_names[] = {
+    "CRITICAL",
+    "ERROR",
+    "WARNING",
+    "INFO",
+    "DEBUG"
+};
+static const size_t _log_level_names_count = sizeof(_log_level_names) / sizeof(_log_level_names[0]);
+static int _log_level = 0;
+static const char *_prog;
+static pid_t _pid;
+
+static void _logv(int level, const char *file, int line, const char *func, const char *fmt, va_list ap)
+{
+    const char *level_name;
+
+    if (level > _log_level)
+        return;
+
+    if (level >= 0 && (size_t)level < _log_level_names_count)
+        level_name = _log_level_names[level];
+    else
+        level_name = NULL;
+
+#ifdef DEBUG
+    if (level_name)
+        fprintf(stderr, "%s[%d] %s: %s:%d %s() ",
+                _prog, _pid, level_name, file, line, func);
+    else
+        fprintf(stderr, "%s[%d] %03d: %s:%d %s() ",
+                _prog, _pid, level, file, line, func);
+#else
+    if (level_name)
+        fprintf(stderr, "%s[%d] %s: ", _prog, _pid, level_name);
+    else
+        fprintf(stderr, "%s[%d] %03d ", _prog, _pid, level);
+    (void)file;
+    (void)line;
+    (void)func;
+#endif
+
+    vfprintf(stderr, fmt, ap);
+    putc('\n', stderr);
+}
+
+int ds_log_get_current_level(void)
+{
+    return _log_level;
+}
+
+void ds_log(int level, const char *file, int line, const char *func, const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    _logv(level, file, line, func, fmt, ap);
+    va_end(ap);
+}
+
+void ds_log_init(const char *argv0)
+{
+    _prog = basename(argv0);
+    _pid = getpid();
+}
---
+++ src/log.h
@@ -0,0 +1,22 @@
+#ifndef __DIETSPLASH_LOG_H_
+#define __DIETSPLASH_LOG_H_
+
+#include <stdarg.h>
+
+void ds_log_init(const char *argv0);
+void ds_log(int level, const char *file, int line, const char *func, const char *fmt, ...);
+int ds_log_get_current_level(void);
+
+#define LOG_CRITICAL  0
+#define LOG_ERROR     1
+#define LOG_WARNING   2
+#define LOG_INFO      3
+#define LOG_DEBUG     4
+
+#define crit(fmt, ...) ds_log(LOG_CRITICAL, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define err(fmt, ...) ds_log(LOG_ERROR, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define wrn(fmt, ...) ds_log(LOG_WARNING, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define inf(fmt, ...) ds_log(LOG_INFO, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+#define dbg(fmt, ...) ds_log(LOG_DEBUG, __FILE__, __LINE__, __FUNCTION__, fmt, ##__VA_ARGS__)
+
+#endif
