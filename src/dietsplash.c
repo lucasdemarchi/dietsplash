6b328bf57df83b3500e7240bda4a251d7e024391
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-10-21 23:04:26 -0200

    Fork me, exec init
    First attempt to wrap ourselves as init. We fork, becoming the child
    process. The father calls exec(init). By now, systemd is hardcoded as
    the init.

--- src/dietsplash.c
+++ src/dietsplash.c
@@ -2,6 +2,12 @@
 #include "log.h"
 
 #include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef REAL_INIT
+# define REAL_INIT "/bin/systemd"
+#endif
 
 struct ds_info {
     struct ds_fb fb;
@@ -12,7 +18,18 @@ static struct ds_info ds_info;
 
 int main(int argc, char *argv[])
 {
+    pid_t pid;
     ds_log_init(argv[0]);
+
+    ds_info.testing = (getpid() != 1);
+
+    inf("running mode -- %s", ds_info.testing ? "testing":"system");
+
+    if (!ds_info.testing) {
+        pid = fork();
+        if (pid)
+            execv(REAL_INIT, argv);
+    }
 
     if (ds_fb_init(&ds_info.fb))
         return 1;
