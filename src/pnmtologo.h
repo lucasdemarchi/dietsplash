1d82c7dfef1af29a0733fc9dfccd9170d47b315e
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-11-04 09:30:03 -0200

    Add license to source files
    Really boring. I'm more than happy to remove all this if someone tells
    me this is not needed in order to enforce GPLv2. For now, keep the
    license in all files.

--- src/pnmtologo.h
+++ src/pnmtologo.h
@@ -1,3 +1,30 @@
+/*
+ *
+ * dietsplash
+ *
+ * Copyright (C) 2010 ProFUSION embedded systems <lucas.demarchi@profusion.mobi>
+ *
+ * pnmtologo.h - Convert a logo in ASCII PNM format to C source suitable for
+ * inclusion in dietsplash. Based on source file for doing this conversion for
+ * the Linux Kernel.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this; see the file COPYING.  If not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ */
+
 #ifndef __DIETSPLASH_PNMTOLOGO_H
 #define __DIETSPLASH_PNMTOLOGO_H
 
